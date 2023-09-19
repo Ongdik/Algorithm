770a2667d92955d26305a2e61ca9617329919fe8
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-09-19 17:21:17 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+ll modVal = 1000000000;
+int dp[101][10]{ 0 };
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= 9; j++) {
+			if (j == 0) dp[i][j] = dp[i - 1][j + 1];
+			else if (j == 9) dp[i][j] = dp[i - 1][j - 1];
+			else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+			dp[i][j] %= modVal;
+		}
+	}
+
+	ll ans = 0;
+	for (int i = 0; i <= 9; i++) {
+		ans = (ans + dp[n][i]) % modVal;
+	}
+	cout << ans;
+}
