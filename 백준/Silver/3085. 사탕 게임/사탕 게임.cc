5ce73de52e7062ea830612e0c514267e76343ad9
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-09-25 10:19:28 +0900

    [Silver II] Title: 사탕 게임, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int maxCount = 0;
+
+void countColumnCandy(char arr[51][51], int size) {
+    for (int i = 0; i < size; i++) {
+        int cnt = 1;
+        for (int j = 0; j < size; j++) {
+            if (arr[i][j] == arr[i][j + 1]) cnt++;
+            else {
+                if (cnt > maxCount) maxCount = cnt;
+                cnt = 1;
+            }
+        }
+    }
+}
+
+void countRowCandy(char arr[51][51], int size) {
+    for (int i = 0; i < size; i++) {
+        int cnt = 1;
+        for (int j = 0; j < size; j++) {
+            if (arr[j][i] == arr[j + 1][i])
+                cnt++;
+            else {
+                if (cnt > maxCount)
+                    maxCount = cnt;
+                cnt = 1;
+            }
+        }
+    }
+}
+
+int main() {
+    int boardSize = 0;
+    char board[51][51];
+
+    cin >> boardSize;
+
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize - 1; j++) {
+            swap(board[i][j], board[i][j + 1]);
+            countRowCandy(board, boardSize);
+            countColumnCandy(board, boardSize);
+            swap(board[i][j + 1], board[i][j]);
+
+            swap(board[j][i], board[j + 1][i]);
+            countRowCandy(board, boardSize);
+            countColumnCandy(board, boardSize);
+            swap(board[j][i], board[j + 1][i]);
+        }
+    }
+
+    cout << maxCount;
+}
