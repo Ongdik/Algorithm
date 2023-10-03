6917196228a3ed457315417446f7be8d82b80263
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-10-03 11:28:25 +0900

    [Silver III] Title: N과 M (7), Time: 496 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> v;
+vector<int> input;
+int n, m;
+
+void makeBT() {
+    if (v.size() > m) return;
+
+    if (v.size() == m) {
+        for (int i : v) {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+
+    for (int i = 0; i < input.size(); i++) {
+        v.push_back(input[i]);
+        makeBT();
+        v.pop_back();
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    cin >> n >> m;
+    while (n--) {
+        int x;
+        cin >> x;
+        input.push_back(x);
+    }
+    sort(input.begin(), input.end());
+    makeBT();
+}
