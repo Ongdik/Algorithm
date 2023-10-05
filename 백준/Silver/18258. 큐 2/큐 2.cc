2c23e422ad2110050f458364445e33773257fe2d
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-10-05 10:51:15 +0900

    [Silver IV] Title: 큐 2, Time: 384 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, x;
+	cin >> n;
+	string str;
+	queue<int> q;
+	while (n--) {
+		cin >> str;
+		if (str == "push") {
+			cin >> x;
+			q.push(x);
+		}
+		else if (str == "pop") {
+			if (q.empty()) cout << -1 << "\n";
+			else {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+		else if (str == "size") {
+			cout << q.size() << "\n";
+		}
+		else if (str == "empty") {
+			if (q.empty()) cout << 1 << "\n";
+			else cout << 0 << "\n";
+		}
+		else if (str == "front") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.front() << "\n";
+		}
+		else if (str == "back") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.back() << "\n";
+		}
+	}
+}
