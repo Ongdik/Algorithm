2483a9f52b2c0b2ac95248943794acd3f0b4a89b
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-10-18 22:52:53 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v, input;
+int numCnt[10001]{ 0 };
+
+void makeBT() {
+	if (v.size() == m) {
+		for (int i : v) cout << i << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < input.size(); i++) {
+		if (!v.empty() && v.back() > input[i]) continue;
+		if (numCnt[input[i]] != 0) {
+			v.push_back(input[i]);
+			numCnt[input[i]]--;
+			makeBT();
+			v.pop_back();
+			numCnt[input[i]]++;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	while (n--) {
+		int data;
+		cin >> data;
+		input.push_back(data);
+		numCnt[data]++;
+	}
+	sort(input.begin(), input.end());
+	input.erase(unique(input.begin(), input.end()), input.end());
+
+	makeBT();
+}
