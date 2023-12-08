ddface7c7d9f9b325d49dd4ea5b877b25decec92
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-12-09 02:37:34 +0900

    [Silver IV] Title: 로프, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int& weight : v) cin >> weight;
+	sort(v.begin(), v.end(), cmp);
+	int ans = v[0], curWeight;
+	for (int i = 1; i < n; i++) {
+		curWeight = v[i] * (i + 1);
+		ans = max(ans, curWeight);
+	}
+	cout << ans;
+}
