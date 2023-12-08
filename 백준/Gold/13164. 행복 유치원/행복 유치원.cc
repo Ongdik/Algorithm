84921048380ab7659bfa4ed7862f04bee8faf368
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-12-09 06:40:23 +0900

    [Gold V] Title: 행복 유치원, Time: 56 ms, Memory: 6396 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, k;
+	cin >> n >> k;
+	vector<int> h(n); vector<int> diff;
+	for (int& height : h) cin >> height;
+	for (int i = 0; i < n - 1; i++) diff.push_back(h[i + 1] - h[i]);
+
+	sort(diff.begin(), diff.end());
+	int ans = 0;
+	for (int i = 0; i < n - k; i++) ans += diff[i];
+	cout << ans;
+}
