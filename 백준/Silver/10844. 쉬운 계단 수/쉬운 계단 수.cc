d169dd363fa6bdf9891d6d225e409611aab41c13
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-12-09 19:33:53 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-#define ll long long
+#define MOD 1000000000
 
-ll modVal = 1000000000;
 int dp[101][10]{ 0 };
 
 int main() {
@@ -12,22 +13,21 @@ int main() {
 
 	int n;
 	cin >> n;
-
-	for (int i = 1; i < 10; i++) {
-		dp[1][i] = 1;
-	}
+	for (int i = 1; i < 10; i++) dp[1][i] = 1;
 	for (int i = 2; i <= n; i++) {
-		for (int j = 0; j <= 9; j++) {
-			if (j == 0) dp[i][j] = dp[i - 1][j + 1];
-			else if (j == 9) dp[i][j] = dp[i - 1][j - 1];
+		for (int j = 0; j < 10; j++) {
+			if (j == 0) dp[i][0] = dp[i - 1][j + 1];
+			else if (j == 9) dp[i][9] = dp[i - 1][j - 1];
 			else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-			dp[i][j] %= modVal;
+			dp[i][j] %= MOD;
 		}
 	}
 
-	ll ans = 0;
-	for (int i = 0; i <= 9; i++) {
-		ans = (ans + dp[n][i]) % modVal;
+	int ans = 0;
+	for (int i = 0; i < 10; i++) {
+		ans = (ans + dp[n][i]) % MOD;
 	}
 	cout << ans;
+
+	return 0;
 }
