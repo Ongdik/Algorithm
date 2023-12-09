9ec04ce6d9fd4b72d07f39f0dca156cc04dba13b
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-12-09 19:01:26 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+
+ll dp[68]{ 1,1,2,4 };
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	for (int i = 4; i < 68; i++) dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4];
+	
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int n;
+		cin >> n;
+		cout << dp[n] << "\n";
+	}
+
+	return 0;
+}
