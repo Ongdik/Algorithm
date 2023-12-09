a0a83e309f8e20d121dadc5a77818140e3f5b1ed
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2023-12-09 18:10:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 12 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -1,28 +1,24 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+#define MOD 1000000009
 #define ll long long
 
+int n;
 ll dp[1000001]{ 0,1,2,4 };
-ll MOD = 1000000009;
-
-void makeDp() {
-	for (int i = 4; i < 1000001; i++) {
-		dp[i] = (dp[i - 3] + dp[i - 2] + dp[i - 1]) % MOD;
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	makeDp();
-
-	int t, n, x;
-	cin >> t;
-	while (t--) {
+	cin >> n;
+	for (int i = 4; i < 1000001; i++) dp[i] = ((dp[i - 1] + dp[i - 2] + dp[i - 3]) % MOD);
+	for (int i = 0; i < n; i++) {
+		int x;
 		cin >> x;
 		cout << dp[x] << "\n";
 	}
-}
 
+	return 0;
+}
