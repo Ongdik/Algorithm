e79697334a25ab87e213e3c50e3769c67a9d1fb6
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-08 15:05:24 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,34 +1,29 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int arr[1001][4]{ 0 };
-int dp[1001][4]{ 0 };
+int arr[1001][3]{ 0 };
+int dp[1001][3]{ 0 };
+
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	int n;
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= 3; j++) {
-			cin >> arr[i][j];
-		}
-	}
 	
-	for (int i = 1; i <= 3; i++) {
-		dp[1][i] = arr[1][i];
+	int n, r, g, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
 	}
 
-	for (int i = 2; i <= n; i++) {
-		dp[i][1] = min(dp[i - 1][2] + arr[i][1], dp[i - 1][3] + arr[i][1]);
-		dp[i][2] = min(dp[i - 1][1] + arr[i][2], dp[i - 1][3] + arr[i][2]);
-		dp[i][3] = min(dp[i - 1][1] + arr[i][3], dp[i - 1][2] + arr[i][3]);
+	dp[0][0] = arr[0][0];
+	dp[0][1] = arr[0][1];
+	dp[0][2] = arr[0][2];
+
+	for (int i = 1; i < n; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][2], dp[i - 1][0]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
 	}
 
-	sort(dp[n] + 1, dp[n] + 4);
-	cout << dp[n][1];
+	cout << min(min(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
 }
-
