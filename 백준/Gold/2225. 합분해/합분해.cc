19c40cf2560384688c46478c80dd3de7408e2a46
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-10 15:18:06 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-#define ll long long
-
-ll MOD = 1000000000;
-ll dp[201][201]{ 0 };
+#define MOD 1000000000
+int dp[201][201]{ 0 };
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -13,17 +11,12 @@ int main() {
 	int n, k;
 	cin >> n >> k;
 
-	for (int i = 1; i <= n; i++) {
-		dp[i][1] = 1;
-	}
-	for (int i = 1; i <= k; i++) {
-		dp[1][i] = i;
-	}
-
-	for (int i = 2; i <= n; i++) {
-		for (int j = 2; j <= k; j++) {
+	for (int i = 0; i <= n; i++) dp[1][i] = 1;
+	for (int i = 1; i <= k; i++) dp[i][0] = 1;
+	for (int i = 2; i <= k; i++) {
+		for (int j = 1; j <= n; j++) {
 			dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
 		}
 	}
-	cout << dp[n][k];
+	cout << dp[k][n];
 }
