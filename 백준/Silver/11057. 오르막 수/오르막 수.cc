3b93b7941db0d98c72834bfca0fb9f3c08211d4f
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-10 14:15:43 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 5936 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.cc
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int dp[1001][10]{ 0 };
+int dp[1001][1001]{ 0 };
 
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -10,25 +9,18 @@ int main() {
 
 	int n;
 	cin >> n;
-
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i <= 9; i++) {
 		dp[1][i] = 1;
 	}
-	for (int i = 1; i <= n; i++) {
-		dp[i][0] = 1;
-	}
-
 	for (int i = 2; i <= n; i++) {
-		for (int j = 1; j < 10; j++) {
-			dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
+		for (int j = 0; j <= 9; j++) {
+			if (j == 0) dp[i][j] = 1;
+			else dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 10007;
 		}
 	}
-
-	int sum = 0;
-	for (int i = 0; i < 10; i++) {
-		sum = (sum + dp[n][i]) % 10007;
+	int ans = 0;
+	for (int i = 0; i <= 9; i++) {
+		ans += dp[n][i];
 	}
-	cout << sum;
-	
+	cout << ans % 10007;
 }
-
