fce6d9d0b9d924cfdc10342dedb13ece03bbe741
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-11 13:40:15 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -3,36 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> v;
-
-void binarySearch(int a, int l, int h) {
-	int idx = (l + h) / 2;
-	if (h < l || idx < 0 || idx > v.size() - 1) {
-		cout << 0 << "\n";
-		return;
-	}
-	
-	if (a > v[idx]) {
-		l = idx + 1;
-		binarySearch(a, l, h);
-	}
-	else if (a < v[idx]) {
-		h = idx - 1;
-		binarySearch(a, l, h);
-	}
-	else {
-		cout << 1 << "\n";
-		return;
-	}
-
-}
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
 	int n, m, x;
 	cin >> n;
+
+	vector<int> v;
 	for (int i = 0; i < n; i++) {
 		cin >> x;
 		v.push_back(x);
@@ -42,6 +20,7 @@ int main() {
 	cin >> m;
 	for (int i = 0; i < m; i++) {
 		cin >> x;
-		binarySearch(x, 0, v.size() - 1);
+		if (binary_search(v.begin(), v.end(), x)) cout << 1 << "\n";
+		else cout << 0 << "\n";
 	}
 }
