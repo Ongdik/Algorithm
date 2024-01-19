c5857ca8947f1e23fd4f03a4ac5f75cb2d388b38
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-19 12:56:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,26 +1,30 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 int arr[1001]{ 0 };
-int dp[1001]{ 0 };
+int dp[1001]{ 1 };
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-		dp[i] = 1;
-	}
-	
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j < i; j++) {
-			if (arr[i] > arr[j]) dp[i] = max(dp[i], dp[j] + 1);
-		}
-	}
-	sort(dp, dp + n);
-	cout << dp[n - 1];
+   int n;
+   cin >> n;
+   for (int i = 0; i < n; i++) {
+      cin >> arr[i];
+      dp[i] = 1;
+   }
+
+   for (int i = 1; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+         if (arr[i] > arr[j]) dp[i] = max(dp[i], dp[j] + 1);
+      }
+   }
+
+   sort(dp, dp + n);
+   cout << dp[n - 1];
+
+   return 0;
 }
