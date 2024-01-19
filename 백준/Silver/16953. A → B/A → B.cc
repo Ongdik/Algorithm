30bbcf24d482224d08985e49818451700faf3777
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-19 12:52:32 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2516 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -8,32 +8,32 @@ using namespace std;
 bool check = false;
 
 void bfs(ll a, ll b) {
-	queue<pair<ll, int>> q;
-	q.push({ a,0 });
+   queue<pair<ll, int>> q;
+   q.push({ a,0 });
 
-	while (!q.empty()) {
-		ll na = q.front().first;
-		int nextCnt = q.front().second + 1;
-		q.pop();
-		if (na > b) continue;
+   while (!q.empty()) {
+      ll na = q.front().first;
+      int nextCnt = q.front().second + 1;
+      q.pop();
+      if (na > b) continue;
 
-		if (na == b) {
-			cout << nextCnt;
-			check = true;
-			return;
-		}
-		
-		q.push({ na * 10 + 1,nextCnt });
-		q.push({ na * 2,nextCnt });
-	}
+      if (na == b) {
+         cout << nextCnt;
+         check = true;
+         return;
+      }
+      
+      q.push({ na * 10 + 1,nextCnt });
+      q.push({ na * 2,nextCnt });
+   }
 }
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
-	ll a, b;
-	cin >> a >> b;
-	bfs(a, b);
-	if (!check) cout << -1;
+   ll a, b;
+   cin >> a >> b;
+   bfs(a, b);
+   if (!check) cout << -1;
 }
