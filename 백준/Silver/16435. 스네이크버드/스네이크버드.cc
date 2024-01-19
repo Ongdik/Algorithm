ebba120c4877c23981c0e1cf49fcfda27995bec0
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-01-19 12:52:04 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.cc
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
-	int n, l;
-	cin >> n >> l;
-	vector<int> h(n);
-	for (int &height : h) cin >> height;
-	sort(h.begin(), h.end());
-	for (int i = 0; i < n; i++) {
-		if (l >= h[i]) l++;
-		else break;
-	}
-	cout << l;
+   int n, l;
+   cin >> n >> l;
+   vector<int> h(n);
+   for (int &height : h) cin >> height;
+   sort(h.begin(), h.end());
+   for (int i = 0; i < n; i++) {
+      if (l >= h[i]) l++;
+      else break;
+   }
+   cout << l;
 }
