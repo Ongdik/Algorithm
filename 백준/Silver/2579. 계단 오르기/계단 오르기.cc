461e47fc92c6aef37ef99acb3cca22fc7172324b
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-05-15 19:32:21 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int arr[301]{ 0 };
-int dp[301]{ 0 };
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n;
+	int arr[301]{ 0 };
+	int dp[301]{ 0 };
+	int n, x;
 	cin >> n;
-
-	for (int i = 1; i <= n; i++) cin >> arr[i];
-
+	for (int i = 1; i <= n; i++) {
+		cin >> x;
+		arr[i] = x;
+	}
 	dp[1] = arr[1];
 	dp[2] = arr[1] + arr[2];
-
-	for (int i = 3; i < n; i++) {
-		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
 	}
-
-	cout << max(dp[n - 2] + arr[n], dp[n - 3] + arr[n - 1] + arr[n]);
+	cout << dp[n];
 }
