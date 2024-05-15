14bbc7cc2d059ce416a9a456ed15180922443a0a
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-05-15 19:10:13 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int dp[12]{ 0,1,2,4 };
+int dp[12]{ 0,1,2,4,7 };
+
+void makeDp() {
+	for (int i = 5; i < 12; i++) {
+		dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	
-	for (int i = 4; i < 12; i++) {
-		dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-	}
 
-	int t;
+	makeDp();
+
+	int t, n;
 	cin >> t;
-	while (t--) {
-		int n;
+	for (int i = 0; i < t; i++) {
 		cin >> n;
 		cout << dp[n] << "\n";
 	}
-
 }
