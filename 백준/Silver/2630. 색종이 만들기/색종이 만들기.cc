394d7007d1a0507fe59388a3a169d3441a40744a
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-10-03 23:35:00 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,38 +1,40 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int arr[128][128]{ 0 };
-int n;
-int zero = 0, one = 0;
+int arr[129][129]{ 0 };
+int ans[2]{ 0 };
 
-void solve(int size, int x, int y) {
-	int num = arr[y][x];
-
-	for (int i = y; i < y + size; i++) {
-		for (int j = x; j < x + size; j++) {
+void solve(int r, int c, int size) {
+	int num = arr[r][c];
+	for (int i = r; i < r + size; i++) {
+		for (int j = c; j < c + size; j++) {
 			if (num != arr[i][j]) {
-				solve(size / 2, x, y);
-				solve(size / 2, x + size / 2, y);
-				solve(size / 2, x, y + size / 2);
-				solve(size / 2, x + size / 2, y + size / 2);
+				solve(r, c, size / 2);
+				solve(r + size / 2, c, size / 2);
+				solve(r, c + size / 2, size / 2);
+				solve(r + size / 2, c + size / 2, size / 2);
 				return;
 			}
 		}
 	}
-	if (num == 0) zero++;
-	else if (num == 1) one++;
+	ans[num]++;
 }
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
+	int n;
 	cin >> n;
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
 		}
 	}
-	solve(n, 0, 0);
-	cout << zero << "\n" << one;
+
+	solve(0, 0, n);
+
+	cout << ans[0] << "\n" << ans[1];
 }
