6ff854d0914eacec95035d66e18344f5bcc9d7df
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-02-21 13:01:39 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -0,0 +1,6 @@
+select count(FI.FISH_TYPE) as FISH_COUNT, FNI.FISH_NAME as FISH_NAME
+from FISH_INFO FI
+join FISH_NAME_INFO FNI
+on FI.FISH_TYPE = FNI.FISH_TYPE
+group by FNI.FISH_NAME
+order by count(FI.FISH_TYPE) desc
