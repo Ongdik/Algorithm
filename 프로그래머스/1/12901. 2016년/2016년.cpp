7678fb514e1060cb43a392c5ce148b35f032f3a2
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2025-04-11 19:37:02 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string answer = "";
+    
+    int sum = 0;
+    
+    string day[7] = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+    int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    for (int i=1; i< a; i++) {
+        sum += month[i];
+    }
+    
+    sum += b - 1;
+    
+    answer = day[sum%7];
+    
+    return answer;
+}
